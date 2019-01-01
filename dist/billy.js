107e2c38c75f79bbed14fee6bcf4e006a77d5ba4
Gary Großgarten <gary.grossgarten@gmail.com>
2019-01-02 00:40:27 +0100

    release 0.3.5

--- dist/billy.js
+++ dist/billy.js
@@ -19,9 +19,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const billy_core_1 = require("@fivethree/billy-core");
 let ExampleApplication = class ExampleApplication {
-    hello_world({ print, gitClean }) {
+    hello_world({ print }) {
         return __awaiter(this, void 0, void 0, function* () {
-            print(this);
+            print('Hello World!');
         });
     }
 };
