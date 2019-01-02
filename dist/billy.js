adbb13fcdcb87d7b8d8bc3e6af62c26800131a74
Gary Großgarten <gary.grossgarten@gmail.com>
2019-01-02 15:11:10 +0100

    bump(0.3.6)

--- dist/billy.js
+++ dist/billy.js
@@ -22,8 +22,8 @@ let ExampleApplication = class ExampleApplication {
     hello_world({ print, bump, push_to_remote }) {
         return __awaiter(this, void 0, void 0, function* () {
             print('Hello World!');
-            bump('0.3.6');
-            push_to_remote();
+            yield bump('0.3.6');
+            yield push_to_remote();
         });
     }
 };
